8689beb8e7c88d6e200ef35bdedecb2c28415022
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-08 19:30:48 +0800

    feat[BlogList]: implement routing to blog details

--- src/BlogList.js
+++ src/BlogList.js
@@ -4,18 +4,24 @@
 //     const blogs = props.blogs;
 //     const title = props.title;
 
+import { Link } from "react-router-dom/cjs/react-router-dom.min"
+
 // destructuring props
 const BlogList = ({blogs, title, handleDeleteBlog}) => {
     return(
         <div className="blog-list">
             <h2>{ title }</h2>
             {blogs.map((blog) => (
-                <div className="blog-preview" key={ blog.id }>
-                    <div className="blog-header">
-                        <h2>Blog { blog.id }: { blog.title }</h2>
+                <div className="blog-row" key={blog.id}>
+                    <div className="blog-preview" key={ blog.id }>
+                        <Link to={`/blogs/${blog.id}`}>
+                            <h2>Blog { blog.id }: { blog.title }</h2>
+                            <p>Written by { blog.author }</p>
+                        </Link>
+                    </div>
+                    <div className="blog-row-delete-button">
                         <button onClick={handleDeleteBlog}>delete</button>
                     </div>
-                    <p>Written by { blog.author }</p>
                 </div>
             ))}
         </div>
