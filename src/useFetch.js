d06c73c56b5df5881309cb97d7d82538cb38ecf8
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-08 18:30:27 +0800

    feat[useFetch]: implement clean up, comment out some console prints

--- src/useFetch.js
+++ src/useFetch.js
@@ -8,14 +8,16 @@ const useFetch = (url) => {
     const [error, setError] = useState(null); // initialize as null to handle error state
 
     useEffect(() => {
-        setTimeout(() => {
-            fetch(url)
+        const abortConstant = new AbortController(); // create an abort controller to cancel fetch request if component unmounts
+
+        setTimeout(() => { // simulate a delay to mimic loading state
+            fetch(url, { signal: abortConstant.signal })
             .then(response => {
-                console.log(response)
+                // console.log(response)
                 if (!response.ok) {
                     throw Error("Could not fetch the data for that resource");
                 }
-                return response.json();
+                return response.json(); // parse the response as JSON
             })
             .then(data => {
                 setError(null); // reset error state before setting blogs
@@ -23,12 +25,20 @@ const useFetch = (url) => {
                 setIsPending(false); // set isPending to false after data is fetched
             })
             .catch(err => {
-                console.error(err);
-                console.log(err.message);
-                setError(err.message); // set error message to state
-                setIsPending(false); // set isPending to false after error
+                // console.error(err);
+                // console.log(err.message);
+                if (err.name === 'AbortError') {
+                    console.log("fetch aborted"); 
+                } else {
+                    setError(err.message); // set error message to state
+                    setIsPending(false); // set isPending to false after error
+                }
             });
         }, 1000);
+
+        // Note: React 18/19’s strict mode may trigger double rendering, which can cause immediate unmount/remount → abort → AbortError.
+
+        return () => { abortConstant.abort(); }; // cleanup function to abort fetch request if component unmounts
     }, [url]); // dependency array to re-run effect when url changes
 
     return { data, isPending, error };
