b950c5ea6b420e4ab229169bb018e1486d4f27f6
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-08 20:06:07 +0800

    feat[BlogDetails]: fetch blog, update component body

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,21 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
-
     const { id } = useParams(); // useParams is used to access the dynamic segments in the URL, in this case, the id of the blog post
+    const { data: blog, isPending, error } = useFetch(`http://localhost:8000/blogs/${id}`);
 
     return ( 
         <div className="blog-details">
-            <h2>Blog Details - { id }</h2>
+            { error && <div style={{color: "red"}}><h2 style={{color: "red"}}>Error fetching blog..</h2></div>} 
+            { isPending && <h2>Loading...</h2> }
+            { blog && 
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p><b>Written by { blog.author }</b></p>
+                    <div>{ blog.body }</div>
+                </article>
+            }
         </div>
      );
 }
