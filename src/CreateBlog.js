112d7cdb17bb82c966d0b23060ecc88839ba090d
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-09 10:17:59 +0800

    feat[CreateBlog]: implement create blog template, css

--- src/CreateBlog.js
+++ src/CreateBlog.js
@@ -1,12 +1,23 @@
-const Create = () => {
+import './css/create-blog.css';
+
+const CreateBlog = () => {
     return (
-        <div className="createBlog">
+        <div className="create-blog">
             <h2>Add a New Blog</h2>
             <form>
-
+                <label>Blog title:</label>
+                <input type="text" required placeholder="Enter blog title:"/>
+                <label>Blog body:</label>
+                <textarea required placeholder="Enter your blog here..."></textarea>
+                <label>Blog author:</label>
+                <select>
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                </select>
+                <button>Add Blog</button>
             </form>
         </div>
       );
 }
  
-export default Create;
+export default CreateBlog;
