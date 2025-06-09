c0280f24c3ac466a89843f6cb46b205e7cb57e84
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-09 15:53:01 +0800

    feat[useDelete]: implement custom hook useDelete

---
+++ src/customHooks/useDelete.js
@@ -0,0 +1,25 @@
+import { useHistory } from 'react-router-dom';
+
+const useDelete = () => {
+    const history = useHistory();
+
+    const handleDelete = (url, blog) => {
+        fetch((url + blog.id), {
+            method: 'DELETE', // specify the HTTP method as DELETE
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw Error("Could not delete the blog post");
+            }
+            console.log(`Blog deleted successfully: ${JSON.stringify(blog)}`);
+            history.push('/'); // navigate to the home page after successful deletion
+        })
+        .catch((err) => {
+            console.error('Error deleting blog:', err);
+            alert(err.message); // alert the user in case of an error
+        });
+    };
+    return handleDelete;
+};
+
+export default useDelete;
