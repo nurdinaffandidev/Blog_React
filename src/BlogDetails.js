ef3d9176751eeae852fd812ff40b880f5c8f2eed
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-09 15:55:03 +0800

    feat[BlogDetails]: implement delete button and functionality, update css for button

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,16 @@
 import './css/blog-details.css';
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./customHooks/useFetch";
 import StringUtils from './utility/StringUtils';
+import useDelete from './customHooks/useDelete';
 
 const BlogDetails = () => {
     const { id } = useParams(); // useParams is used to access the dynamic segments in the URL, in this case, the id of the blog post
-    const { data: blog, isPending, error } = useFetch(`http://localhost:8000/blogs/${id}`);
-
+    const basePath = "http://localhost:8000/blogs/"; // base path for the API endpoint
+    // url: `http://localhost:8000/blogs/${id}`
+    const { data: blog, isPending, error } = useFetch(basePath + id); // useFetch is a custom hook to fetch data from the API`);
+    const handleDeleteClick = useDelete(); // custom hook to handle delete operation
+    
     return ( 
         <div className="blog-details">
             { error && <div style={{color: "red"}}><h2 style={{color: "red"}}>Error fetching blog..</h2></div>} 
@@ -16,6 +20,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p><b>Written by { StringUtils.capitalize(blog.author) }</b></p>
                     <div>{ blog.body }</div>
+                    <button onClick={ () => handleDeleteClick(basePath, blog) }>Delete</button>
                 </article>
             }
         </div>
